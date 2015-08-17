9cf17b002051fca0b69452784e95d741ccc0ddd4
davidglez3 <davidglez3@gmail.com>
2015-08-17 16:08:13 +0200

    Base de datos
    Añadimos la conexión con la base de datos

---
+++ Data/globals.php
@@ -0,0 +1,23 @@
+<?php
+class config {
+	//Función que devuelve el nombre del servidor de la Base de Datos
+	public static function getDBServer(){
+		return 'localhost';
+	}
+
+	//Función que devuelve el nombre de la Base de Datos
+	public static function getDBName(){
+		return 'marmoleria';
+	}
+
+	//Función que devuelve el usuario de la Base de Datos
+	public static function getDBUser(){
+		return 'root';
+	}
+
+	//Función que devuelve la contraseña de la Base de Datos
+	public static function getDBPwd(){
+		return '';
+	}
+}
+?>
